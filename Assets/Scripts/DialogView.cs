94ecab5c8b4799ef04325df7fbaacc8ba027017e
RedFox3DMaker <146384683+RedFox3DMaker@users.noreply.github.com>
2025-11-27 23:27:52 +0100

    creation of NPCDialog prefab

---
+++ Assets/Scripts/DialogView.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+    
+public class DialogView : MonoBehaviour
+{
+    // UI element references
+    Label m_CharacterName;
+    Label m_CharacterLine;
+    
+    void OnEnable()
+    {
+        // The UXML is already instantiated by the UIDocument component
+        var uiDocument = GetComponent<UIDocument>();
+        //m_DialogController.InitializeItemList(uiDocument.rootVisualElement, m_ListEntryTemplate);
+
+        var root = uiDocument.rootVisualElement;
+        m_CharacterName = root.Q<Label>("CharacterName");
+        m_CharacterLine = root.Q<Label>("CharacterLine");
+
+        m_CharacterName.text = "My Character";
+        m_CharacterLine.text = "Hello !";
+    }
+}
