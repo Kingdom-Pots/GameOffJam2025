4162fb475d5715f27ce526d60ee71b58a14b7359
kamaleon70 <mauro.casu@gmail.com>
2025-11-29 18:40:48 -0500

    Added factions status

--- Assets/Gameplay/Scripts/TaxCollector/FactionOnHover.cs
+++ Assets/Gameplay/Scripts/TaxCollector/FactionOnHover.cs
@@ -3,21 +3,61 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using System.Linq;
+using System.Collections.Generic;
 
 public class FactionOnHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public GameObject popup; // Assign your popup text GameObject in Inspector
 
+    public static bool selectionEnabled = true;
+
+    public GameObject confirmSelection = null;
+    public TextMeshProUGUI textConfirmSelection = null;
+    public static string lastSelectedFaction = "";
+
+    public void Start()
+    {
+        //Button btn = confirmSelection.transform.Find("ButtonOK")?.GetComponent<Button>();
+        var selfObject = gameObject;
+        Button btn = GetComponents<Button>()[0];
+        btn.onClick.AddListener(() => {
+            SelectionRequest();
+        });       
+    }
+
     public void OnPointerEnter(PointerEventData eventData)
     {
-        gameObject.GetComponent<UnityEngine.UI.Image>().color = Color.green;
-        popup.GetComponents<TextMeshProUGUI>()[0].text = gameObject.name;
+        if (!FactionOnHover.selectionEnabled) return;
+        if (FactionService.FactionSelected == null)
+        {
+            gameObject.GetComponent<UnityEngine.UI.Image>().color = Color.green;
+            lastSelectedFaction = gameObject.name;
+            textConfirmSelection.text = textConfirmSelection.text.Replace("<chosen faction>", gameObject.name);        
+            
+        }
         popup.SetActive(true);
+        popup.GetComponents<TextMeshProUGUI>()[0].text = gameObject.name;
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        gameObject.GetComponent<UnityEngine.UI.Image>().color = Color.white;
-        popup.SetActive(false);
+        if (!FactionOnHover.selectionEnabled) return;
+        if (FactionService.FactionSelected == null)
+        {
+            gameObject.GetComponent<UnityEngine.UI.Image>().color = Color.white;
+        }
+        popup.SetActive(false);        
     }
+
+    void SelectionRequest()
+    {
+        if (FactionService.FactionSelected == null)
+        {
+            confirmSelection.SetActive(true);
+            FactionOnHover.selectionEnabled = false;
+        }
+    }
+
+    
 }
--- Assets/Gameplay/Scripts/TaxCollector/TaxCollectorHelper.cs
+++ Assets/Gameplay/Scripts/TaxCollector/TaxCollectorHelper.cs
@@ -1,8 +1,11 @@
+using System.Collections.Generic;
 using MoreMountains.Feedbacks;
 using UnityEngine;
+using TMPro;
 
 public class TaxCollectorHelper : MonoBehaviour
 {
+    public GameObject factionsPanel = null;
     GameManager gm = null;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -25,4 +28,24 @@ public class TaxCollectorHelper : MonoBehaviour
             gm.CursorUnlock();
         }
     }
+
+    public void SetFactionsValues()
+    {
+        StartCoroutine(FactionService.GetFactions(OnFactionsLoaded));
+    }
+
+    void OnFactionsLoaded(List<Faction> factions)
+    {
+        foreach (var faction in factions)
+        {
+            var factionsToSet = factionsPanel.GetComponentsInChildren<TextMeshProUGUI>();
+            foreach (var a in factionsToSet)
+            {
+                if (a.transform.parent.name == faction.factionname)
+                {
+                    a.text = faction.total.ToString();
+                }
+            }
+        }
+    }          
 }
